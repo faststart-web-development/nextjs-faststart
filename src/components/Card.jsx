d6a2546378fd5ceec7892243e475c01b7b04b457
Daniel Koudouna <daniel.koudouna@gmail.com>
2023-03-06 19:26:36 +0000

    Clean up card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,19 +1,4 @@
 export default function Card(props) {
-
-
-
-  // let my_burgers = 10;
-  // function BurgerComponent(burgers) {
-  //   return <h1>I have {burgers} Burgers</h1>;
-  // };
-  // let final_html = BurgerComponent(my_burgers);
-
-
-
-  // console.log(component, BurgerComponent, final_html);
-
-
-
   return (
     <a href={props.link} className="card">
       <h2>{props.title} &rarr;</h2>
